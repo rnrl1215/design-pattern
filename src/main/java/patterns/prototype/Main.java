51d6a74847cf5104a061df601feff5eb33268f5b
skahn <d1215@naver.com>
2021-11-24 16:00:35 +0900

    added prototype pattern

---
+++ src/main/java/patterns/prototype/Main.java
@@ -0,0 +1,11 @@
+package patterns.prototype;
+
+public class Main {
+    public static void main(String[] args) throws CloneNotSupportedException {
+        Circle circle1 = new Circle(10, 20,8);
+        Circle circle2 = circle1.copy();
+
+        System.out.println("circle1.X" + circle1.getX());
+        System.out.println("circle2.X" + circle2.getX());
+    }
+}
