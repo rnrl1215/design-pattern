491109144a6d5cf4e27c07149f7e60571a1df7c1
skahn <d1215@naver.com>
2021-11-29 15:27:57 +0900

    update code

--- src/main/java/patterns/abstractfacotry/Main.java
+++ src/main/java/patterns/abstractfacotry/Main.java
@@ -9,6 +9,7 @@ import patterns.abstractfacotry.car.Wheel;
 
 public class Main {
     public static void main(String[] args) {
+        // 간단한 변경으로 다른 종류의 car 를 만들수 있다.
         CarFactory carFactory = new BenzFactory();
         //CarFactory carFactory = new BMWFactory();
 
