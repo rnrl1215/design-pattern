6fc69ea4ce4150c2f2fde9cd39796740b4790d51
skahn <d1215@naver.com>
2021-11-30 14:07:52 +0900

    apply Factory Method Pattern to Abstract Method Pattern

--- src/main/java/patterns/abstractfacotry/Main.java
+++ src/main/java/patterns/abstractfacotry/Main.java
@@ -10,12 +10,16 @@ import patterns.abstractfacotry.car.Wheel;
 public class Main {
     public static void main(String[] args) {
         // 간단한 변경으로 다른 종류의 car 를 만들수 있다.
+        /*
         CarFactory benzCarFactory = new BenzFactory();
         CarFactory BMWCarFactory = new BMWFactory();
+         */
 
-        Frame body = benzCarFactory.createFrame();
-        Wheel wheel = benzCarFactory.createWheel();
-        Engine engine = BMWCarFactory.createEngine();
+        CarFactory carFactory =  CarFactoryMethod.getInstance(1);
+
+        Frame body = carFactory.createFrame();
+        Wheel wheel = carFactory.createWheel();
+        Engine engine = carFactory.createEngine();
 
         body.info();
         wheel.info();
